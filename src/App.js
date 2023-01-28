98bc96274a0c16847febd10ada0e190a02919f1d
sojants18password <sojants18@gmail.com>
2023-01-28 22:41:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import SunEditor from 'suneditor-react';
+import 'suneditor/dist/css/suneditor.min.css'; 
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState();
+
+  console.log(value);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<SunEditor
+setContents={value}
+onChange={setValue}
+setOptions={{
+  buttonList: [
+    ["undo", "redo"],
+    ["font", "fontSize", "formatBlock"],
+    [
+      "bold",
+      "underline",
+      "italic",
+      "strike",
+      "subscript",
+      "superscript"
+    ],
+    ["fontColor", "hiliteColor"],
+    ["align", "list", "lineHeight"],
+    ["outdent", "indent"],
+
+    ["table", "horizontalRule", "link", "image", "video", "audio"],
+
+    ["preview", "print", "codeView"],
+    ["removeFormat"],
+
+  ], 
+  defaultTag: "div",
+  minHeight: "300px",
+  showPathLabel: false,
+  formats:  [
+    'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
+  ],
+
+}}
+/>
+</>
   );
 }
 
